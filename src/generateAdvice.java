a34abc2a4d3264d22538e5a56671b763a2e6fd17
tdecker10 <22thayne.decker@gmail.com>
2025-05-05 18:46:22 -0500

    thayneGenerate

---
+++ src/generateAdvice.java
@@ -0,0 +1,7 @@
+public class generateAdvice {
+    
+    public double[] createAdvice(double[] goalPercentages, double[] wants, double[] needs, double[] savings){
+        double[] stats = generateStats.createStats(wants, needs, savings, goalPercentages);
+    }
+    
+}
